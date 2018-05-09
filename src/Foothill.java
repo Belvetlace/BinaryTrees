1007e1b838b9a8358bd3f7313d11a4b359d59ccb
Belvetlace <belvet.lace@gmail.com>
2018-05-08 18:25:39 -0700

    main restructure

--- src/Foothill.java
+++ src/Foothill.java
@@ -3,6 +3,8 @@
 
 import cs_1c.*;
 
+import java.util.NoSuchElementException;
+
 class PrintObject<E> implements Traverser<E>
 {
    public void visit(E x)
@@ -21,163 +23,189 @@ public class Foothill
       FHlazySearchTree<Integer> searchTree = new FHlazySearchTree<Integer>();
       PrintObject<Integer> intPrinter = new PrintObject<Integer>();
 
-      searchTree.traverse(intPrinter);
+      System.out.println("\nInitial size: " );
+      printTreeSizes(searchTree);
+      searchTree = populateTree(searchTree);
 
-      System.out.println("\ninitial size: " + searchTree.size());
+      System.out.println("After populating -- traversal and sizes: ");
+      printTreeSizes(searchTree);
 
+      System.out.println("Collecting garbage on new tree - should be " +
+              "no garbage.");
+      searchTree.collectGarbage();
+      printTreeSizes(searchTree);
+
+      // test assignment operator
+      FHlazySearchTree<Integer> searchTree2
+              = (FHlazySearchTree<Integer>) searchTree.clone();
+      System.out.println("\nCloning tree: tree 2:");
+      printTreeSizes(searchTree2);
+
+      System.out.println("\n\nAttempting 1 removal on 1st tree: ");
+      if (searchTree.remove(20))
+         System.out.println("removed " + 20);
+      printTreeSizes(searchTree);
+
+      System.out.println("Collecting Garbage - should clean 1 item. ");
+      searchTree.collectGarbage();
+      printTreeSizes(searchTree);
+
+      System.out.println("Collecting Garbage again - no change expected. ");
+      searchTree.collectGarbage();
+      printTreeSizes(searchTree);
+
+      // test soft insertion and deletion:
+
+      System.out.println("Adding 'hard' 22 - should see new sizes. ");
+      searchTree.insert(22);
+      printTreeSizes(searchTree);
+
+      System.out.println("\nAfter soft removal 22. ");
+      searchTree.remove(22);
+      printTreeSizes(searchTree);
+
+      System.out.println("Repeating soft removal 22. Should see no change. ");
+      searchTree.remove(22);
+      printTreeSizes(searchTree);
+
+      System.out.println("Soft insertion 22. Hard size should not change. ");
+      searchTree.insert(22);
+      printTreeSizes(searchTree);
+
+      System.out.println("\nSoft removal 22. ");
+      searchTree.remove(22);
+
+      System.out.println("\n----------------------");
+      System.out.println("\nTest Find() on filled tree");
+      testFind(searchTree);
+      System.out.println("\n----------------------");
+
+      System.out.println("\nTest findMax() on filled tree");
+      testFindMax(searchTree);
+      System.out.println("\nSoft delete 70");
+      searchTree.remove(70);
+      testFindMax(searchTree);
+      System.out.println("\n----------------------");
+
+      System.out.println("\nInserting extra 100 nodes " +
+              "and attempting 100 removals: ");
+      insertExtraHundredNodes(searchTree);
+      System.out.print("removed:");
+      for (k = 170; k > 0; k--)
+      {
+         if (searchTree.remove(k))
+            System.out.print(" " + k);
+      }
+
+      System.out.println("\nsearch_tree now:");
+      printTreeSizes(searchTree);
+
+      searchTree.collectGarbage();
+
+      System.out.println("\nsearch_tree now after garbage collector:");
+      printTreeSizes(searchTree);
+
+      System.out.println("\n----------------------");
+      System.out.println("\nTest Find() on empty tree (should not be there)");
+      testFind(searchTree);
+      System.out.println("\n----------------------");
+
+      System.out.println("\nTest findMax() on empty tree");
+      testFindMax(searchTree);
+      System.out.println("\n----------------------");
+
+      insertExtraNodes(searchTree2);
+
+      System.out.println("\nsearchTree2:");
+      printTreeSizes(searchTree2);
+
+   }
+
+   private static void testFindMax(FHlazySearchTree<Integer> searchTree)
+   {
+      printTreeSizes(searchTree);
+      try
+      {
+         System.out.println("search tree for max: found "
+                 + searchTree.findMax());
+      } catch (NoSuchElementException e)
+      {
+         System.out.println("max not found");
+      }
+   }
+
+   // helper methods
+   public static FHlazySearchTree<Integer>
+   populateTree(FHlazySearchTree<Integer> searchTree)
+   {
       searchTree.insert(50);
       searchTree.insert(20);
       searchTree.insert(30);
       searchTree.insert(70);
       searchTree.insert(10);
       searchTree.insert(60);
+      return searchTree;
+   }
 
-      System.out.println("After populating -- traversal and sizes: ");
+   public static void printTreeSizes(FHlazySearchTree<Integer> searchTree)
+   {
+      PrintObject<Integer> intPrinter = new PrintObject<Integer>();
       searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
+      System.out.println("\nTree size: " + searchTree.size()
               + "  Hard size: " + searchTree.sizeHard());
+   }
 
-      System.out.println("Collecting garbage on new tree - should be " +
-              "no garbage.");
-      searchTree.collectGarbage();
-      System.out.println("tree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
+   public static void insertExtraNodes(FHlazySearchTree<Integer> searchTree)
+   {
+      searchTree.insert(500);
+      searchTree.insert(200);
+      searchTree.insert(300);
+      searchTree.insert(700);
+      searchTree.insert(100);
+      searchTree.insert(600);
+   }
 
-      // test assignment operator
-      FHlazySearchTree<Integer> searchTree2
-              = (FHlazySearchTree<Integer>) searchTree.clone();
-      System.out.println("\nCloning tree: ");
-      System.out.println("tree 2 size: " + searchTree2.size()
-              + "  Hard size: " + searchTree2.sizeHard());
-      searchTree2.traverse(intPrinter);
-
-
-      System.out.println("\n\nAttempting 1 removal: ");
-      if (searchTree.remove(20))
-         System.out.println("removed " + 20);
-      System.out.println("tree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("Collecting Garbage - should clean 1 item. ");
-      searchTree.collectGarbage();
-      //searchTree.removeHard(searchTree.mRoot,20); // testing hard remove in absence of garbage collector
-      System.out.println("tree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("Collecting Garbage again - no change expected. ");
-      searchTree.collectGarbage();
-      System.out.println("tree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      // test soft insertion and deletion:
-
-      System.out.println("Adding 'hard' 22 - should see new sizes. ");
-      searchTree.insert(22);
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("\nAfter soft removal 22. ");
-      searchTree.remove(22);
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("Repeating soft removal 22. Should see no change. ");
-      searchTree.remove(22);
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("Soft insertion 22. Hard size should not change. ");
-      searchTree.insert(22);
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      System.out.println("\n\nAttempting 100 removals: ");
-      for (k = 70; k > 0; k--)
+   public static void
+   insertExtraHundredNodes(FHlazySearchTree<Integer> searchTree)
+   {
+      for(int i = 71 ; i < 171; i++)
+      searchTree.insert(i);
+   }
+   public static void testFind(FHlazySearchTree<Integer> searchTree)
+   {
+      System.out.println("\nSearch for existed node 30");
+      try
       {
-         if (searchTree.remove(k))
-            System.out.println("removed " + k);
+         System.out.println("search tree1 for 30: found "
+                 + searchTree.find(30));
+      } catch (NoSuchElementException e)
+      {
+         System.out.println("node 30 not found");
       }
 
-      System.out.println("\nsearch_tree now:");
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      searchTree.collectGarbage();
-
-      System.out.println("\nsearch_tree now:");
-      searchTree.traverse(intPrinter);
-      System.out.println("\ntree 1 size: " + searchTree.size()
-              + "  Hard size: " + searchTree.sizeHard());
-
-      searchTree2.insert(500);
-      searchTree2.insert(200);
-      searchTree2.insert(300);
-      searchTree2.insert(700);
-      searchTree2.insert(100);
-      searchTree2.insert(600);
-      System.out.println("\nsearchTree2:");
-      searchTree2.traverse(intPrinter);
-      System.out.println("\ntree 2 size: " + searchTree2.size()
-              + "  Hard size: " + searchTree2.sizeHard());
-
-      //System.out.println("\nsearch_tree removing last node:");
-      //searchTree.remove(70);
-      //searchTree.collectGarbage();
+      System.out.println("\nSearch for soft deleted node 22");
+      try
+      {
+         System.out.println("search tree1 for 22: found "
+                 + searchTree.find(22));
+      } catch (NoSuchElementException e)
+      {
+         System.out.println("node 22 not found");
+      }
+      System.out.println("\nSearch for deleted node 20");
+      try
+      {
+         System.out.println("search tree1 for 20: found "
+                 + searchTree.find(20));
+      } catch (NoSuchElementException e)
+      {
+         System.out.println("node 20 not found");
+      }
    }
 }
 
 
 /* ---------------------- Run --------------------------
 
-initial size: 0
-After populating -- traversal and sizes: 
-10 20 30 50 60 70 
-tree 1 size: 6  Hard size: 6
-Collecting garbage on new tree - should be no garbage.
-tree 1 size: 6  Hard size: 6
 
-
-Attempting 1 removal: 
-removed 20
-tree 1 size: 5  Hard size: 6
-Collecting Garbage - should clean 1 item. 
-tree 1 size: 5  Hard size: 5
-Collecting Garbage again - no change expected. 
-tree 1 size: 5  Hard size: 5
-Adding 'hard' 22 - should see new sizes. 
-10 22 30 50 60 70 
-tree 1 size: 6  Hard size: 6
-
-After soft removal. 
-10 30 50 60 70 
-tree 1 size: 5  Hard size: 6
-Repeating soft removal. Should see no change. 
-10 30 50 60 70 
-tree 1 size: 5  Hard size: 6
-Soft insertion. Hard size should not change. 
-10 22 30 50 60 70 
-tree 1 size: 6  Hard size: 6
-
-
-Attempting 100 removals: 
-removed 70
-removed 60
-removed 50
-removed 30
-removed 22
-removed 10
-
-search_tree now:
-
-tree 1 size: 0  Hard size: 0
-
-searchTree2:
-
-10 20 30 50 60 70 100 200 300 500 600 700 
-tree 2 size: 12  Hard size: 12
 ---------------------------------------------------------------------- */
